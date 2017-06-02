b9ddd6e0a6af3670a67a857ce07a53efc532c5dd
Aleksander Spro <aleksander.spro@xledger.com>
2017-06-02 13:41:55 +0200

    Fix query to handle empty search terms

--- TheCollection.Web/Services/SearchableQuery.cs
+++ TheCollection.Web/Services/SearchableQuery.cs
@@ -9,19 +9,24 @@ namespace TheCollection.Web.Services
     {
         public static SqlQuerySpec Create(string collectionId, IEnumerable<string> searchTerms, int top = 0)
         {
-            var counter = 0;
             var topSelect = top > 0 ? $"TOP {top}" : "";
-            var searchterms = searchTerms.Select(term => $"CONTAINS(o.{nameof(ISearchable.SearchString).ToLower()}, @param{counter++})").ToArray();
-            var query = $"SELECT {topSelect} VALUE o FROM {collectionId} o WHERE {searchterms.Aggregate((current, next) => current + " AND " + next)}";
+            var query = $"SELECT {topSelect} VALUE o FROM {collectionId} o WHERE 1=1 {CreateSearchTermWhereClause(searchTerms)}";
             return new SqlQuerySpec { QueryText = query, Parameters = CreateParams(searchTerms) };
         }
 
         public static SqlQuerySpec Count(string collectionId, IEnumerable<string> searchTerms)
         {
-            var counter = 0;
-            var searchterms = searchTerms.Select(term => $"CONTAINS(o.{nameof(ISearchable.SearchString).ToLower()}, @param{counter++})").ToArray();
-            var query = $"SELECT COUNT(1) as Count FROM {collectionId} o WHERE {searchterms.Aggregate((current, next) => current + " AND " + next)}";
+            var query = $"SELECT COUNT(1) as Count FROM {collectionId} o WHERE 1=1 {CreateSearchTermWhereClause(searchTerms)}";
             return new SqlQuerySpec { QueryText = query, Parameters = CreateParams(searchTerms) };
+        }
+
+        static string CreateSearchTermWhereClause(IEnumerable<string> searchTerms)
+        {
+            var counter = 0;
+            var searchterms = searchTerms.Where(term => term.Length > 0).Select(term => $"CONTAINS(o.{nameof(ISearchable.SearchString).ToLower()}, @param{counter++})").ToArray();
+            if (counter > 0) return $"AND {searchterms.Aggregate((current, next) => $"{current} AND {next}")}";
+
+            return "";
         }
 
         static SqlParameterCollection CreateParams(IEnumerable<string> searchTerms)
