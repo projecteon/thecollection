16560d4681cf57aaecdbbe1af067083d7751c0c8
Aleksander Spro <gledesrus@hotmail.com>
2017-06-08 22:55:56 +0200

    Add start of login page

---
+++ TheCollection.Web/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TheCollection.Web.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Login(string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+    }
+}
