72aba4b8508249a5773b912fbe6f16bf5887081c
Aleksander Spro <aleksander.spro@xledger.com>
2017-07-21 11:44:02 +0200

    Update to bootstrap 4

--- TheCollection.Web/ClientApp/components/NavMenu.tsx
+++ TheCollection.Web/ClientApp/components/NavMenu.tsx
@@ -3,43 +3,37 @@ import { Link } from 'react-router';
 
 export class NavMenu extends React.Component<any, void> {
     public render() {
-        return <div className='main-nav'>
-                <div className='navbar navbar-inverse'>
-                <div className='navbar-header'>
-                    <button type='button' className='navbar-toggle' data-toggle='collapse' data-target='.navbar-collapse'>
-                        <span className='sr-only'>Toggle navigation</span>
-                        <span className='icon-bar'></span>
-                        <span className='icon-bar'></span>
-                        <span className='icon-bar'></span>
+        return  <div className='main-nav'>
+                  <nav className='navbar flex-column navbar-toggleable-md navbar-inverse'>
+                    <button className='navbar-toggler navbar-toggler-right' type='button' data-toggle='collapse' data-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
+                      <span className='navbar-toggler-icon'></span>
                     </button>
-                    <Link className='navbar-brand' to={ '/' }><img src='/images/teapot.svg' width='30' height='30' alt='' style={{display: 'inline-block', verticalAlign: 'bottom', marginRight: 5}}/>The Collection</Link>
-                </div>
-                <div className='clearfix'></div>
-                <div className='navbar-collapse collapse'>
-                    <ul className='nav navbar-nav'>
-                        <li>
-                            <Link to={ '/' } activeClassName='active'>
-                                <span className='glyphicon glyphicon-home'></span> Home
-                            </Link>
+                    <Link className='navbar-brand' to={ '/' }><img src='/images/teapot.svg' width='30' height='30' alt='' className='d-inline-block align-top' />The Collection</Link>
+                    <div className='collapse navbar-collapse' id='navbarNav'>
+                      <ul className='nav navbar-nav flex-column'>
+                        <li className='nav-item'>
+                          <Link to={ '/' } activeClassName='active'>
+                            <span className='fa fa-home'></span> Home
+                          </Link>
                         </li>
-                         <li>
-                            <Link to={ '/teabags' } activeClassName='active'>
-                                <span className='glyphicon glyphicon-th-list'></span> Teabags
-                            </Link>
+                        <li className='nav-item'>
+                          <Link to={ '/teabags' } activeClassName='active'>
+                            <span className='fa fa-th-list'></span> Teabags
+                          </Link>
                         </li>
-                        <li>
-                            <Link to={ '/teabagform' } activeClassName='active'>
-                                <span className='glyphicon glyphicon-plus'></span> New teabag
-                            </Link>
+                        <li className='nav-item'>
+                          <Link to={ '/teabagform' } activeClassName='active'>
+                            <span className='fa fa-plus'></span> New teabag
+                          </Link>
                         </li>
-                        <li>
-                            <Link to='/Account/LogOff'>
-                                <span className='glyphicon glyphicon-log-out'></span> Log off
-                            </Link>
+                        <li className='nav-item'>
+                          <Link to='/Account/LogOff'>
+                            <span className='fa fa-sign-out'></span> Log off
+                          </Link>
                         </li>
-                    </ul>
-                </div>
-            </div>
-        </div>;
+                      </ul>
+                    </div>
+                  </nav>
+                </div>;
     }
 }
