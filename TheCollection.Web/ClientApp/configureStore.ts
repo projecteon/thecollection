b1af8ddbd92dca01e2e93280fe9318175ea04f94
Aleksander Spro <aleksander.spro@xledger.com>
2017-07-25 15:59:36 +0200

    Fix tab alignment

--- TheCollection.Web/ClientApp/configureStore.ts
+++ TheCollection.Web/ClientApp/configureStore.ts
@@ -4,30 +4,30 @@ import { routerReducer } from 'react-router-redux';
 import * as Store from './store';
 
 export default function configureStore(initialState?: Store.IApplicationState) {
-    // Build middleware. These are functions that can process the actions before they reach the store.
-    const windowIfDefined = typeof window === 'undefined' ? null : window as any;
-    // If devTools is installed, connect to it
-    const devToolsExtension = windowIfDefined && windowIfDefined.devToolsExtension as () => GenericStoreEnhancer;
-    const createStoreWithMiddleware = compose(
-        applyMiddleware(thunk),
-        devToolsExtension ? devToolsExtension() : f => f
-    )(createStore);
+  // Build middleware. These are functions that can process the actions before they reach the store.
+  const windowIfDefined = typeof window === 'undefined' ? null : window as any;
+  // If devTools is installed, connect to it
+  const devToolsExtension = windowIfDefined && windowIfDefined.devToolsExtension as () => GenericStoreEnhancer;
+  const createStoreWithMiddleware = compose(
+    applyMiddleware(thunk),
+    devToolsExtension ? devToolsExtension() : f => f
+  )(createStore);
 
-    // Combine all reducers and instantiate the app-wide store instance
-    const allReducers = buildRootReducer(Store.reducers);
-    const store = createStoreWithMiddleware(allReducers, initialState) as Redux.Store<Store.IApplicationState>;
+  // Combine all reducers and instantiate the app-wide store instance
+  const allReducers = buildRootReducer(Store.reducers);
+  const store = createStoreWithMiddleware(allReducers, initialState) as Redux.Store<Store.IApplicationState>;
 
-    // Enable Webpack hot module replacement for reducers
-    if (module.hot) {
-        module.hot.accept('./store', () => {
-            const nextRootReducer = require<typeof Store>('./store');
-            store.replaceReducer(buildRootReducer(nextRootReducer.reducers));
-        });
-    }
+  // Enable Webpack hot module replacement for reducers
+  if (module.hot) {
+    module.hot.accept('./store', () => {
+      const nextRootReducer = require<typeof Store>('./store');
+      store.replaceReducer(buildRootReducer(nextRootReducer.reducers));
+    });
+  }
 
-    return store;
+  return store;
 }
 
 function buildRootReducer(allReducers) {
-    return combineReducers<Store.IApplicationState>(Object.assign({}, allReducers, { routing: routerReducer }));
+  return combineReducers<Store.IApplicationState>(Object.assign({}, allReducers, { routing: routerReducer }));
 }
