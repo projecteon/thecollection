8a5a4a34847680495f9c637dfebcf7e593f2b188
Aleksander Spro <aleksander.spro@xledger.com>
2017-08-08 20:27:53 +0200

    Try using types instead of interfaces

---
+++ TheCollection.Web/ClientApp/types/Teabag.ts
@@ -0,0 +1,13 @@
+import { RefValue } from './RefValue';
+
+export type Teabag = {
+  id: string;
+  brand: RefValue;
+  serie: string;
+  flavour: string;
+  hallmark: string;
+  type: RefValue;
+  country: RefValue;
+  serialnumber: string;
+  imageid: string;
+}
