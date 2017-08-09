ebbea2a51f0deb0fe2b978b1bdd26cdbee3c6fd1
Aleksander Spro <gledesrus@hotmail.com>
2017-08-09 17:07:49 +0200

    Codemaid

--- TheCollection.Web/ClientApp/components/Loader.tsx
+++ TheCollection.Web/ClientApp/components/Loader.tsx
@@ -2,7 +2,8 @@ import * as React from 'react';
 
 import './Loader.css';
 
-// other optiosn: https://projects.lukehaas.me/css-loaders/
+// other options:
+// https://projects.lukehaas.me/css-loaders/
 // https://codepen.io/martinvd/pen/xbQJom
 export const Loader : React.StatelessComponent<{}> = props =>
     <div style={{position: 'absolute', height: '100vh', width: '100%', backgroundColor: 'rgba(255, 255, 255, 0.7)', zIndex: 5}} onClick={event => event.stopPropagation()}><div className="loader"><div className="inner one" /><div className="inner two" /><div className="inner three" /></div></div>;
