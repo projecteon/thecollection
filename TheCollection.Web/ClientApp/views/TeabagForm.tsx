50da21c99b2ba74f38a366640df549078fb6ab3f
Aleksander Spro <abspro@gmail.com>
2017-09-12 22:12:11 +0200

    Trying a different layout for form

--- TheCollection.Web/ClientApp/views/TeabagForm.tsx
+++ TheCollection.Web/ClientApp/views/TeabagForm.tsx
@@ -113,50 +113,54 @@ class TeabagForm extends React.Component<TeabagProps, {}> {
 
   renderForm() {
     return  <form className='form-horizontal' style={{marginTop: 10}}>
-              <div className='form-group'>
-                <div className='col-sm-offset-2 col-sm-10'>
-                  <Image imageid={this.props.teabag.imageid}/>
+              <div className="row">
+                <div className='col-sm-6 flex-first'>
+                  <div className='form-group'>
+                    <Image imageid={this.props.teabag.imageid}/>
+                  </div>
                 </div>
-              </div>
-              <BrandInputGroupItem  inputid='inputBrand'
-                                    responsiveInputComponentWidth='col-sm-6'
-                                    label='Brand'
-                                    onAddNew={this.onAddNewBrand}
-                                    onSearch={this.onSearchBrand}
-                                    onItemSelected={this.props.changeBrand}
-                                    onClear={this.props.clearBrand}
-                                    renderItem={this.renderBrandComboBoxItem}
-                                    displayProperty={'name'}
-                                    selectedItem={this.props.teabag.brand}/>
-              <InputFormGroupItem inputid='inputFlavour' responsiveInputComponentWidth='col-sm-6' label='Flavour' placeholder='Flavour' value={this.props.teabag.flavour} onChange={this.props.changeFlavour}/>
-              <InputFormGroupItem inputid='inputSeries' responsiveInputComponentWidth='col-sm-6' label='Series' placeholder='Series' value={this.props.teabag.serie} onChange={this.props.changeSerie}/>
-              <InputFormGroupItem inputid='inputSerialnumber' responsiveInputComponentWidth='col-sm-3' label='Serialnumber' placeholder='Serialnumber' value={this.props.teabag.serialnumber} onChange={this.props.changeSerialNumber}/>
-              <TextareaFormGroupItem inputid='inputHallmark' responsiveInputComponentWidth='col-sm-6' label='Hallmark' placeholder='Hallmark' value={this.props.teabag.hallmark} onChange={this.props.changeHallmark}/>
-              <BagtypeInputGroupItem  inputid='inputBagType'
-                                      responsiveInputComponentWidth='col-sm-6'
-                                      label='Type'
-                                      onAddNew={this.onAddNewBagType}
-                                      onSearch={this.onSearchType}
-                                      onItemSelected={this.props.changeBagtype}
-                                      onClear={this.props.clearBagtype}
-                                      renderItem={this.renderBagTypeComboBoxItem}
-                                      displayProperty={'name'}
-                                      selectedItem={this.props.teabag.bagtype} />
-              <CountryInputGroupItem  inputid='inputCountry'
-                                      responsiveInputComponentWidth='col-sm-6'
-                                      label='Country'
-                                      onAddNew={this.onAddNewCountry}
-                                      onSearch={this.onSearchCountry}
-                                      onItemSelected={this.props.changeCountry}
-                                      onClear={this.props.clearCountry}
-                                      renderItem={this.renderCountryComboBoxItem}
-                                      displayProperty={'name'}
-                                      selectedItem={this.props.teabag.country} />
-              <div className='form-group'>
-                <div className='col-sm-offset-2 col-sm-10'>
-                  <button className='btn btn-default' style={{cusor: 'pointer'}} onClick={this.onSave}>
-                    <span className='fa fa-floppy-o' aria-hidden='true'></span> Save
-                  </button>
+                <div className='col-sm-6 flex-last'>
+                  <BrandInputGroupItem  inputid='inputBrand'
+                                        responsiveInputComponentWidth='col-sm-12'
+                                        label='Brand'
+                                        onAddNew={this.onAddNewBrand}
+                                        onSearch={this.onSearchBrand}
+                                        onItemSelected={this.props.changeBrand}
+                                        onClear={this.props.clearBrand}
+                                        renderItem={this.renderBrandComboBoxItem}
+                                        displayProperty={'name'}
+                                        selectedItem={this.props.teabag.brand}/>
+                  <InputFormGroupItem inputid='inputFlavour' responsiveInputComponentWidth='col-sm-12' label='Flavour' placeholder='Flavour' value={this.props.teabag.flavour} onChange={this.props.changeFlavour}/>
+                  <InputFormGroupItem inputid='inputSeries' responsiveInputComponentWidth='col-sm-12' label='Series' placeholder='Series' value={this.props.teabag.serie} onChange={this.props.changeSerie}/>
+                  <InputFormGroupItem inputid='inputSerialnumber' responsiveInputComponentWidth='col-sm-6' label='Serialnumber' placeholder='Serialnumber' value={this.props.teabag.serialnumber} onChange={this.props.changeSerialNumber}/>
+                  <TextareaFormGroupItem inputid='inputHallmark' responsiveInputComponentWidth='col-sm-12' label='Hallmark' placeholder='Hallmark' value={this.props.teabag.hallmark} onChange={this.props.changeHallmark}/>
+                  <BagtypeInputGroupItem  inputid='inputBagType'
+                                          responsiveInputComponentWidth='col-sm-12'
+                                          label='Type'
+                                          onAddNew={this.onAddNewBagType}
+                                          onSearch={this.onSearchType}
+                                          onItemSelected={this.props.changeBagtype}
+                                          onClear={this.props.clearBagtype}
+                                          renderItem={this.renderBagTypeComboBoxItem}
+                                          displayProperty={'name'}
+                                          selectedItem={this.props.teabag.bagtype} />
+                  <CountryInputGroupItem  inputid='inputCountry'
+                                          responsiveInputComponentWidth='col-sm-12'
+                                          label='Country'
+                                          onAddNew={this.onAddNewCountry}
+                                          onSearch={this.onSearchCountry}
+                                          onItemSelected={this.props.changeCountry}
+                                          onClear={this.props.clearCountry}
+                                          renderItem={this.renderCountryComboBoxItem}
+                                          displayProperty={'name'}
+                                          selectedItem={this.props.teabag.country} />
+                  <div className='form-group'>
+                    <div className='col-sm-offset-2 col-sm-10'>
+                      <button className='btn btn-default' style={{cusor: 'pointer'}} onClick={this.onSave}>
+                        <span className='fa fa-floppy-o' aria-hidden='true'></span> Save
+                      </button>
+                    </div>
+                  </div>
                 </div>
               </div>
             </form>;
