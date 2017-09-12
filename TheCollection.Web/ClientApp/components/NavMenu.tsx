210c861e14ff934202692dbd7a1120402c7f743e
Aleksander Spro <abspro@gmail.com>
2017-09-12 22:18:19 +0200

    Remove some nav menu items that should not be needed

--- TheCollection.Web/ClientApp/components/NavMenu.tsx
+++ TheCollection.Web/ClientApp/components/NavMenu.tsx
@@ -27,21 +27,6 @@ export class NavMenu extends React.Component<any, {}> {
                           </Link>
                         </li>
                         <li className='nav-item'>
-                          <Link to={ '/brandform' } activeClassName='active'>
-                            <span className='fa fa-plus'></span> New brand
-                          </Link>
-                        </li>
-                        <li className='nav-item'>
-                          <Link to={ '/bagtypeform' } activeClassName='active'>
-                            <span className='fa fa-plus'></span> New bagtype
-                          </Link>
-                        </li>
-                        <li className='nav-item'>
-                          <Link to={ '/countryform' } activeClassName='active'>
-                            <span className='fa fa-plus'></span> New country
-                          </Link>
-                        </li>
-                        <li className='nav-item'>
                           <form action='/Account/LogOff/' method='post'>
                             <button className='btn btn-link' type='submit'><span className='fa fa-sign-out'></span> Log off</button>
                           </form>
