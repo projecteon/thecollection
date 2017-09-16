bf799a02b0d4e02abbb192fdbf57c0e1f364b4b0
Aleksander Spro <abspro@gmail.com>
2017-09-16 13:45:44 +0200

    Use scss for styles Put component with their styles (scss)

--- TheCollection.Web/ClientApp/components/NavMenu.tsx
+++ TheCollection.Web/ClientApp/components/NavMenu.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { Link } from 'react-router';
 
+import './NavMenu.scss';
+
 export class NavMenu extends React.Component<any, {}> {
     public render() {
         return  <div className='main-nav'>
