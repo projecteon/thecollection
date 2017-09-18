b9d5a80c42ad0a7495f254c8979fd9b7a85d9327
Aleksander Spro <abspro@gmail.com>
2017-09-18 22:38:59 +0200

    Add a few more tests

---
+++ TheCollection.Business.Tests.Unit/Tea/BagTypeTagsTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using TheCollection.Business.Tea;
+using Xunit;
+
+namespace TheCollection.Business.Tests.Unit.Tea
+{
+    [Trait("BagType", "Tags are created")]
+    public class BagTypeTagsTests
+    {
+        BagType BagType { get; }
+        Searchable SearchableBrand { get; }
+
+        public BagTypeTagsTests()
+        {
+            BagType = new BagType { Id = System.Guid.NewGuid().ToString(), Name = "Paper" };
+            SearchableBrand = new Searchable(BagType);
+        }
+
+        [Fact(DisplayName = "Tag contains name in lowercase")]
+        public void ContainsNameLowerCase()
+        {
+            Assert.True(SearchableBrand.Tags.Any(tag => tag == BagType.Name.ToLower()));
+        }
+
+        [Fact(DisplayName = "Tag does not contain Id")]
+        public void NotContainsId()
+        {
+            Assert.False(SearchableBrand.Tags.Any(tag => tag.ToLower() == BagType.Id.ToLower()));
+        }
+    }
+}
---
+++ TheCollection.Business.Tests.Unit/Tea/CountryTagsTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using TheCollection.Business.Tea;
+using Xunit;
+
+namespace TheCollection.Business.Tests.Unit.Tea
+{
+    [Trait("Country", "Tags are created")]
+    public class CountryTagsTests
+    {
+        Country Country { get; }
+        Searchable SearchableBrand { get; }
+
+        public CountryTagsTests()
+        {
+            Country = new Country { Id = System.Guid.NewGuid().ToString(), Name = "Norway" };
+            SearchableBrand = new Searchable(Country);
+        }
+
+        [Fact(DisplayName = "Tag contains name in lowercase")]
+        public void ContainsNameLowerCase()
+        {
+            Assert.True(SearchableBrand.Tags.Any(tag => tag == Country.Name.ToLower()));
+        }
+
+        [Fact(DisplayName = "Tag does not contain Id")]
+        public void NotContainsId()
+        {
+            Assert.False(SearchableBrand.Tags.Any(tag => tag.ToLower() == Country.Id.ToLower()));
+        }
+    }
+}
