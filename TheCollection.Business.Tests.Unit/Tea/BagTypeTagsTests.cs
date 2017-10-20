92b732e53ab0d4c8dacc4fd2af823688d1968931
Aleksander Spro <abspro@gmail.com>
2017-10-20 14:52:58 +0200

    Rename to fit model name

--- TheCollection.Business.Tests.Unit/Tea/BagTypeTagsTests.cs
+++ TheCollection.Business.Tests.Unit/Tea/BagTypeTagsTests.cs
@@ -8,24 +8,24 @@ namespace TheCollection.Business.Tests.Unit.Tea
     public class BagTypeTagsTests
     {
         BagType BagType { get; }
-        Searchable SearchableBrand { get; }
+        Searchable SearchableBagType { get; }
 
         public BagTypeTagsTests()
         {
             BagType = new BagType { Id = System.Guid.NewGuid().ToString(), Name = "Paper" };
-            SearchableBrand = new Searchable(BagType);
+            SearchableBagType = new Searchable(BagType);
         }
 
         [Fact(DisplayName = "Tag contains name in lowercase")]
         public void ContainsNameLowerCase()
         {
-            Assert.True(SearchableBrand.Tags.Any(tag => tag == BagType.Name.ToLower()));
+            Assert.True(SearchableBagType.Tags.Any(tag => tag == BagType.Name.ToLower()));
         }
 
         [Fact(DisplayName = "Tag does not contain Id")]
         public void NotContainsId()
         {
-            Assert.False(SearchableBrand.Tags.Any(tag => tag.ToLower() == BagType.Id.ToLower()));
+            Assert.False(SearchableBagType.Tags.Any(tag => tag.ToLower() == BagType.Id.ToLower()));
         }
     }
 }
--- TheCollection.Business.Tests.Unit/Tea/CountryTagsTests.cs
+++ TheCollection.Business.Tests.Unit/Tea/CountryTagsTests.cs
@@ -8,24 +8,24 @@ namespace TheCollection.Business.Tests.Unit.Tea
     public class CountryTagsTests
     {
         Country Country { get; }
-        Searchable SearchableBrand { get; }
+        Searchable SearchableCountry { get; }
 
         public CountryTagsTests()
         {
             Country = new Country { Id = System.Guid.NewGuid().ToString(), Name = "Norway" };
-            SearchableBrand = new Searchable(Country);
+            SearchableCountry = new Searchable(Country);
         }
 
         [Fact(DisplayName = "Tag contains name in lowercase")]
         public void ContainsNameLowerCase()
         {
-            Assert.True(SearchableBrand.Tags.Any(tag => tag == Country.Name.ToLower()));
+            Assert.True(SearchableCountry.Tags.Any(tag => tag == Country.Name.ToLower()));
         }
 
         [Fact(DisplayName = "Tag does not contain Id")]
         public void NotContainsId()
         {
-            Assert.False(SearchableBrand.Tags.Any(tag => tag.ToLower() == Country.Id.ToLower()));
+            Assert.False(SearchableCountry.Tags.Any(tag => tag.ToLower() == Country.Id.ToLower()));
         }
     }
 }
