8a99b721d3909f9469b260e334baf9805dd2667e
Aleksander Spro <abspro@gmail.com>
2017-11-12 16:04:11 +0100

    dont have margins on menu bar

--- TheCollection.Web/ClientApp/components/Layout.tsx
+++ TheCollection.Web/ClientApp/components/Layout.tsx
@@ -3,12 +3,13 @@ import NavMenu from './NavMenu';
 
 type LayoutProps = {
     body: React.ReactElement<any>;
-}
+};
 
+// tslint:disable-next-line:variable-name
 const Layout: React.StatelessComponent<LayoutProps> = props => {
   return  <div>
-            <div className='row' style={{position: 'relative'}}>
-                <div className='col-sm-12 col-md-3' style={{zIndex: 5, position: 'sticky', top: 0}}>
+            <div className='row' style={{position: 'relative', margin: 0}}>
+                <div className='col-sm-12 col-md-3' style={{zIndex: 5, position: 'sticky', top: 0, padding: 0}}>
                     <NavMenu />
                 </div>
                 <div className='col-sm-12 col-md-9'>
@@ -16,6 +17,6 @@ const Layout: React.StatelessComponent<LayoutProps> = props => {
                 </div>
             </div>
           </div>;
-}
+};
 
 export default Layout;
