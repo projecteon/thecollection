55ae45ab17d7ef015fd5f56ccfdbea626c415634
Aleksander Spro <abspro@gmail.com>
2017-12-05 21:09:30 +0100

    Removed dead code

--- TheCollection.Business/Period.cs
+++ TheCollection.Business/Period.cs
@@ -24,13 +24,5 @@ namespace TheCollection.Business {
 
         public int Year { get; }
         public int Month { get; }
-
-        public int CompareTo(Period that) {
-            if (Year > that.Year) return -1;
-            if (Year == that.Year) return 0;
-            if (Month > that.Month) return -1;
-            if (Month == that.Month) return 0;
-            return 1;
-        }
     }
 }
