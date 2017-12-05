6d2d40abb773b02ad2c4059bf9806ff5e0fe73fc
Aleksander Spro <abspro@gmail.com>
2017-12-05 21:16:55 +0100

    Added tests for RefValueComparer

---
+++ TheCollection.Business.Tests.Unit/RefValueComparerTests.cs
@@ -0,0 +1,51 @@
+namespace TheCollection.Business.Tests.Unit {
+    using TheCollection.Business;
+    using Xunit;
+
+    [Trait("RefValueComparerTests", "RefValueComparer equality tests")]
+    public class RefValueComparerTests {
+        [Fact(DisplayName = "When RefValues are same object then return true")]
+        public void WhenRefValuesAreSameObjectThenReturnTrue() {
+            var refValue = new RefValue { Id = "someid", Name = "somename" };
+            var comparer = new RefValueComparer();
+
+            Assert.True(comparer.Equals(refValue, refValue));
+        }
+
+        [Fact(DisplayName = "When RefValues have the same id and name then return true")]
+        public void WhenRefValuesHaveTheSameIdAndNameThenReturnTrue() {
+            var refValue = new RefValue { Id = "someid", Name = "somename" };
+            var refValue2 = new RefValue { Id = "someid", Name = "somename" };
+            var comparer = new RefValueComparer();
+
+            Assert.True(comparer.Equals(refValue, refValue2));
+        }
+
+        [Fact(DisplayName = "When RefValues have the same id but different name then return true")]
+        public void WhenRefValuesHaveTheSameIdButDifferentNameThenReturnTrue() {
+            var refValue = new RefValue { Id = "someid", Name = "somename" };
+            var refValue2 = new RefValue { Id = "someid", Name = "somename2" };
+            var comparer = new RefValueComparer();
+
+            Assert.True(comparer.Equals(refValue, refValue2));
+        }
+
+        [Fact(DisplayName = "When RefValues have different ids and name then return false")]
+        public void WhenRefValuesHaveDifferentIdsAndNameThenReturnTrue() {
+            var refValue = new RefValue { Id = "someid", Name = "somename" };
+            var refValue2 = new RefValue { Id = "someid2", Name = "somename2" };
+            var comparer = new RefValueComparer();
+
+            Assert.False(comparer.Equals(refValue, refValue2));
+        }
+
+        [Fact(DisplayName = "When RefValues have different ids but same name then return false")]
+        public void WhenRefValuesHaveDifferentIdsButSameNameThenReturnTrue() {
+            var refValue = new RefValue { Id = "someid", Name = "somename" };
+            var refValue2 = new RefValue { Id = "someid2", Name = "somename" };
+            var comparer = new RefValueComparer();
+
+            Assert.False(comparer.Equals(refValue, refValue2));
+        }
+    }
+}
