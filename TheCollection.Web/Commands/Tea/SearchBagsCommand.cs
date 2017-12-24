da14f894494f74f3d1f7ee44bd69caa4bb428bc5
Aleksander Spro <abspro@gmail.com>
2017-12-24 16:31:43 +0100

    Codemaid

--- TheCollection.Web/Commands/Tea/SearchBagsCommand.cs
+++ TheCollection.Web/Commands/Tea/SearchBagsCommand.cs
@@ -1,21 +1,19 @@
 namespace TheCollection.Web.Commands.Tea {
-
     using System.Linq;
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.Azure.Documents;
+    using TheCollection.Data.DocumentDB;
     using TheCollection.Domain.Extensions;
     using TheCollection.Domain.Tea;
-    using TheCollection.Data.DocumentDB;
     using TheCollection.Web.Constants;
+    using TheCollection.Web.Contracts;
     using TheCollection.Web.Extensions;
     using TheCollection.Web.Models;
     using TheCollection.Web.Translators;
     using TheCollection.Web.Translators.Tea;
-    using TheCollection.Web.Contracts;
 
     public class SearchBagsCommand : IAsyncCommand<Search> {
-
         public SearchBagsCommand(IDocumentClient documentDbClient, IApplicationUser applicationUser) {
             DocumentDbClient = documentDbClient;
             BagTranslator = new BagToBagTranslator(applicationUser);
